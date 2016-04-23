8bad374c5f5701f6cf45356fcb414a54c7fdb796
Bohdan <bohdan.fedys@gmail.com>
2016-04-24 00:16:39 +0300

    Add comments, readme

--- sorttable.js
+++ sorttable.js
@@ -8,17 +8,17 @@
 
 jQuery.fn.addSortWidget = function(options){
 	var defaults = {
-		img_asc: "img\\asc_sort.gif",	
-        img_desc: "img\\desc_sort.gif",	
-		img_nosort: "img\\no_sort.gif",		
+		img_asc: "img/asc_sort.gif",	
+        img_desc: "img/desc_sort.gif",	
+		img_nosort: "img/no_sort.gif",		
 	};
 	
 	var options = $.extend({}, defaults, options),
 		$destElement = $(this),
         is_asc = true;
 		
-	$("th", $destElement).each(function(index){
-        $("<img>")
+	$("th", $destElement).each(function(index){ // to each header cell (index is useful while sorting)
+        $("<img>")                              // create image that allows you to sort by specific column 
             .attr('src', options.img_nosort)
             .addClass('sorttable_img')
             .css({
@@ -26,26 +26,27 @@ jQuery.fn.addSortWidget = function(options){
                 'margin-left': '10px',
             })
             .on('click', function(){
-                $(".sorttable_img", $destElement).attr('src', options.img_nosort);
+                $(".sorttable_img", $destElement).attr('src', options.img_nosort); 
                 $(this).attr('src', (is_asc) ? options.img_desc : options.img_asc);
                 is_asc = !is_asc;
                 
-                var rows = $("tr", $destElement).not(":has(th)").get();
-                rows.sort(function(a, b){
-                    var m = $("td:eq(" + index + ")", a).text();
+                var rows = $("tr", $destElement).not(":has(th)").get(); // save all rows (tr) into array (.get())
+                rows.sort(function(a, b){               
+                    // sort array with table rows
+                    var m = $("td:eq(" + index + ")", a).text(); // get column you needed by using index of th element (closure)
                     var n = $("td:eq(" + index + ")", b).text();
                     if (is_asc)
-                        return m.localeCompare(n);
+                        return m.localeCompare(n); // asc
                     else
-                        return n.localeCompare(m);
+                        return n.localeCompare(m); // desc
                 });
                 
-                var tbody = ($destElement.has("tbody")) ? "tbody" : "";
+                var tbody = ($destElement.has("tbody")) ? "tbody" : ""; // check if table has tbody
                 for (var i=0; i<rows.length; i++){
-                    $(tbody, $destElement).append(rows[i]);
+                    $(tbody, $destElement).append(rows[i]); // add each row to table (elements do not duplicate, just place to new position)
                 }
             })
-            .appendTo(this);
+            .appendTo(this); // add created sort image with click event handler to current th element
     });
 	
 	return $destElement;
